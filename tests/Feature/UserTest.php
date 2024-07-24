bdde87349797141e3b2b83f6f8ef2e100212aa02
elmojerry88 <taysonsevvani@gmail.com>
2024-07-24 11:51:26 +0100

    test(user): add gett all users, login, logout,, destroy and update

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -11,7 +11,42 @@ class UserTest extends TestCase
     /**
      * A basic feature test example.
      */
-    public function test_example(): void
+    public function get_all_user_test(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    public function login_user_test(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    public function logout_user_test(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    public function create_user_test(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    public function update_user_test(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    public function destroy_user_test(): void
     {
         $response = $this->get('/');
 
