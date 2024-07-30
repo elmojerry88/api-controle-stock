739f95a4c99ac7ade0cf5ecc70c88b57f61ed3d9
elmojerry88 <taysonsevvani@gmail.com>
2024-07-30 22:30:38 +0100

    feat(routes): add employees, vehicle, equipment, deliveries routes and Employee request validation

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
 
 
-
-Route::controller(UserController::class)->prefix('user')->group(function () {
+Route::controller(\App\Http\Controllers\UserController::class)->prefix('user')->group(function () {
     Route::get('/', 'index');
     Route::post('/update/{id}', 'update');
     Route::post('/create','store');
@@ -15,6 +13,40 @@ Route::controller(UserController::class)->prefix('user')->group(function () {
     Route::post('/logout', 'logout')->middleware(['auth:sanctum']);
 });
 
+Route::controller(\App\Http\Controllers\EmployeeController::class)->prefix('employee')->group(function () {
+    Route::get('/', 'index');
+    Route::post('/update/{id}', 'update');
+    Route::post('/create','store');
+    Route::delete('/delete/{id}', 'destroy');
+});
+
+Route::controller(\App\Http\Controllers\EquipmentController::class)->prefix('equipment')->group(function () {
+    Route::get('/', 'index');
+    Route::post('/update/{id}', 'update');
+    Route::post('/create','store');
+    Route::delete('/delete/{id}', 'destroy');
+});
+
+Route::controller(\App\Http\Controllers\VehicleController::class)->prefix('vehicle')->group(function () {
+    Route::get('/', 'index');
+    Route::post('/update/{id}', 'update');
+    Route::post('/create','store');
+    Route::delete('/delete/{id}', 'destroy');
+});
+
+Route::controller(\App\Http\Controllers\DeliveriesEqupmentController::class)->prefix('delivery/equipment')->group(function () {
+    Route::get('/', 'index');
+    Route::post('/update/{id}', 'update');
+    Route::post('/create','store');
+    Route::delete('/delete/{id}', 'destroy');
+});
+
+Route::controller(\App\Http\Controllers\DeliveriesVehicleController::class)->prefix('delivery/vehicle')->group(function () {
+    Route::get('/', 'index');
+    Route::post('/update/{id}', 'update');
+    Route::post('/create','store');
+    Route::delete('/delete/{id}', 'destroy');
+});
 
 
 
