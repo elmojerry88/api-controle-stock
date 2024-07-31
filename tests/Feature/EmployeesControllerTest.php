f914ab2af2cd140fda2fe151f0afa0a00548ae4b
elmojerry88 <taysonsevvani@gmail.com>
2024-07-31 09:49:00 +0100

    feat(employees): add factory and test

--- tests/Feature/EmployeesControllerTest.php
+++ tests/Feature/EmployeesControllerTest.php
@@ -5,16 +5,36 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Illuminate\Testing\Fluent\AssertableJson;
 
 class EmployeesControllerTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
+   use RefreshDatabase;
+
     public function test_get_all_employees(): void
     {
-        $response = $this->get('/');
+        \App\Models\Employees::factory()->createOne();
+
+        $response = $this->get('/api/employee');
 
         $response->assertStatus(200);
+
+        $response->assertJsonStructure([
+            'name',
+            'email',
+            'phone',
+        ]);
+
+        $response->assertJson(fn (AssertableJson $json) =>
+
+        $json->whereAllType([
+            '0.name' => 'string',
+            '0.email' => 'sting',
+            '0.phone' => 'integer'
+         ])
+    
+    );
+
+
     }
 }
