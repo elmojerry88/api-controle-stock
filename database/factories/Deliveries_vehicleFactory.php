8828acad75645e21c1a63377fba0d1d32a157986
elmojerry88 <taysonsevvani@gmail.com>
2024-08-14 21:40:54 +0100

    feat(delivery-vehicle): change deliveries factory name

--- database/factories/Deliveries_vehicleFactory.php
+++ database/factories/Deliveries_vehicleFactory.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Deliveries_vehicle>
  */
-class DeliveryVehiclesFactory extends Factory
+class Deliveries_vehicleFactory extends Factory
 {
     /**
      * Define the model's default state.
